cfe2fd9183ceea55266b57b62bb0a22a17f89469
Meesam <meesam.engineer@gmail.com>
2023-11-24 12:57:53 +0530

    implement extention method

---
+++ ExtensionMethod/DemoExtensionClass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethod
+{
+    public static class DemoExtensionClass
+    {
+        public static void PrintListElement(this List<int> ints)
+        { 
+           if(ints != null)
+            {
+                foreach(int i in ints)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
--- ExtensionMethod/Program.cs
+++ ExtensionMethod/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ExtensionMethod;
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello Extension");
+        List<int> list = new List<int>() { 1, 2, 3 };
+        list.PrintListElement();
+        
+    }
+}
